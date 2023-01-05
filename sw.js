c996f3a0dc36a7355fa93ee9ad117f6a19306962
hakujoo <grisette100@gmail.com>
2023-01-05 12:03:10 +0100

    up

--- sw.js
+++ sw.js
@@ -6,7 +6,11 @@ const addResourcesToCache = async (resources) => {
 self.addEventListener("install", (event) => {
     event.waitUntil(
         addResourcesToCache([
-            "/my-pwa",
+            "/my-pwa/",
+            "/my-pwa/index.html",
+            "/my-pwa/style.css",
+            "/my-pwa/app.js",
+            "/my-pwa/potato.png",
         ])
     );
 });
