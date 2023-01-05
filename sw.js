cf745cfdda292afe314408c097a959620a4a27a2
hakujoo <grisette100@gmail.com>
2023-01-05 11:59:28 +0100

    up

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const addResourcesToCache = async (resources) => {
 self.addEventListener("install", (event) => {
     event.waitUntil(
         addResourcesToCache([
-            "/",
+            "/my-pwa",
         ])
     );
 });
