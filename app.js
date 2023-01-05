72a8197803d943f401b1bb8f0f27ee0730d5bbdc
hakujoo <grisette100@gmail.com>
2023-01-05 11:46:07 +0100

    up

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const registerServiceWorker = async () => {
     if ("serviceWorker" in navigator) {
         try {
-            const registration = await navigator.serviceWorker.register("/my-pwa/sw.js", {
-                scope: "/my-pwa/",
+            const registration = await navigator.serviceWorker.register("sw.js", {
+                scope: "/",
             });
             if (registration.installing) {
                 console.log("Installation du service worker en cours");
